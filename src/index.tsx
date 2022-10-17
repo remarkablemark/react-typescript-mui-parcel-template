f36009dc8b4b517e795302abb4a7c656317b0be9
Mark <mark@remarkablemark.org>
2022-10-16 22:45:16 -0400

    feat: add CSS baseline
    https://mui.com/material-ui/react-css-baseline/#global-reset

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import '@fontsource/roboto/700.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './components/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>
 );
